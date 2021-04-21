ea780aa51df7c955242ac819a34a271a67322083
Josh Carswell <josh.carswell@thecarswells.ca>
2021-04-21 13:49:23 +0000

    Initail commit

---
+++ dztech_dz60rgb_v2/j/keymap.c
@@ -0,0 +1,181 @@
+#include QMK_KEYBOARD_H
+#define _QWERTY 0
+#define _COLMAK 1
+#define _DVORAK 2
+#define _MOD0 3
+#define _MOD1 4
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+		[_QWERTY] = LAYOUT( /* Base QWERTY */
+			KC_GESC,         KC_1,    KC_2,            KC_3,    KC_4,    KC_5,   KC_6,     KC_7,    KC_8,    KC_9,    KC_0,            KC_MINS, KC_EQL,  KC_BSPC,
+		    KC_TAB,          KC_Q,    KC_W,            KC_E,    KC_R,    KC_T,   KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,            KC_LBRC, KC_RBRC, KC_BSLASH,
+			CTL_T(KC_CAPS),  KC_A,    KC_S,            KC_D,    KC_F,    KC_G,   KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN,         KC_QUOT,          KC_ENT,
+		    KC_LSFT,         KC_Z,    KC_X,            KC_C,    KC_V,    KC_B,   KC_N,     KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), KC_UP,            KC_DEL,
+		    KC_LCTL,        KC_LGUI,  KC_LALT,                   KC_SPC,                    MO(_MOD0),   KC_RCTL, KC_LEFT,         KC_DOWN,          KC_RIGHT),
+		[_COLMAK] = LAYOUT( /* Base COLMAK*/
+			KC_GESC,         KC_1,    KC_2,            KC_3,    KC_4,    KC_5,   KC_6,     KC_7,    KC_8,    KC_9,    KC_0,            KC_MINS, KC_EQL,  KC_BSPC,
+		    KC_TAB,          KC_Q,    KC_W,            KC_F,    KC_P,    KC_G,   KC_J,     KC_L,    KC_U,    KC_Y,    KC_SCLN,         KC_LBRC, KC_RBRC, KC_BSLASH,
+			CTL_T(KC_CAPS),  KC_A,    KC_R,            KC_S,    KC_T,    KC_D,   KC_H,     KC_N,    KC_E,    KC_I,    KC_O,            KC_QUOT,          KC_ENT,
+		    KC_LSFT,         KC_Z,    KC_X,            KC_C,    KC_V,    KC_B,   KC_K,     KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), KC_UP,            KC_DEL,
+		    KC_LCTL,        KC_LGUI,  KC_LALT,                   KC_SPC,                    MO(_MOD0),   KC_RCTL, KC_LEFT,         KC_DOWN,          KC_RIGHT),
+		[_DVORAK] = LAYOUT( /* Base DVORAK */
+			KC_GESC,         KC_1,    KC_2,            KC_3,    KC_4,    KC_5,   KC_6,     KC_7,    KC_8,    KC_9,    KC_0,            KC_LBRC, KC_RBRC, KC_BSPC,
+		    KC_TAB,          KC_QUOT, KC_COMM,         KC_DOT,  KC_P,    KC_Y,   KC_F,     KC_G,    KC_C,    KC_R,    KC_L,            KC_SLSH, KC_EQL,  KC_BSLASH,
+			CTL_T(KC_CAPS),  KC_A,    KC_O,            KC_E,    KC_U,    KC_I,   KC_D,     KC_H,    KC_T,    KC_N,    KC_S,            KC_MINS,          KC_ENT,
+		    KC_LSFT,         KC_SCLN, KC_Q,            KC_J,    KC_K,    KC_X,   KC_B,     KC_M,    KC_W,    KC_V,    KC_Z,            KC_UP,            KC_RSFT,
+		    KC_LCTL,        KC_LGUI,  KC_LALT,                  KC_SPC,                    MO(_MOD0),   KC_RCTL, KC_LEFT,         KC_DOWN,          KC_RIGHT),
+		[_MOD0] = LAYOUT( /* FN */
+		    KC_GRV,          KC_F1,   KC_F2,           KC_F3,   KC_F4,   KC_F5,  KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,          KC_F11,  KC_F12, _______,
+		    _______,         _______, _______,         _______, _______, _______, _______, _______, _______, _______, _______,         KC_PGDN, KC_PGUP, RESET,
+		    _______,         _______, _______,         _______, _______, _______, _______, _______, _______, _______, _______,         TO(_MOD1),        EEP_RST,
+		    _______,         _______, _______,         _______, _______, _______, _______, _______, KC_HOME, KC_END,  _______,         KC_VOLU,          KC_INS,
+			_______,         _______, _______,                           KC_MPLY,                   _______, _______, KC_MPRV,         KC_VOLD,          KC_MNXT),
+		[_MOD1] = LAYOUT( /* CONTROL */
+			_______,         KC_F1,       KC_F2,       KC_F3,       KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,          KC_F11,  KC_F12, _______,
+			_______,         TO(_QWERTY), _______,     _______,     _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, RGB_MOD,         _______, _______, RESET,
+			_______,         _______,     TO(_DVORAK), _______,     _______, _______, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, RGB_RMOD,         _______,          EEP_RST,
+			_______,         _______,     _______,     TO(_COLMAK), _______, _______, _______, _______, _______, _______, _______,         _______,          _______,
+			_______,         _______,     _______,                           _______,                   _______, _______, _______,         _______,          _______),
+		};
+
+void rgb_matrix_indicators_user(void) {
+	  uint8_t this_led = host_keyboard_leds();
+	  if (!g_suspend_state) {
+	    switch (biton32(layer_state)) {
+        case _MOD0:
+          if ( this_led & (1<<USB_LED_NUM_LOCK)) {
+            rgb_matrix_set_color(13, 0xFF, 0x00, 0x00);
+          } else {
+            rgb_matrix_set_color(13, 0x00, 0x00, 0x00);
+          }
+          rgb_matrix_set_color(0, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(1, 0x00, 0xFF, 0x00);
+          rgb_matrix_set_color(2, 0x00, 0xFF, 0x00);
+          rgb_matrix_set_color(3, 0x00, 0xFF, 0x00);
+          rgb_matrix_set_color(4, 0x00, 0xFF, 0x00);
+          rgb_matrix_set_color(5, 0x00, 0xFF, 0x00);
+          rgb_matrix_set_color(6, 0x00, 0xFF, 0x00);
+          rgb_matrix_set_color(7, 0x00, 0xFF, 0x00);
+          rgb_matrix_set_color(8, 0x00, 0xFF, 0x00);
+          rgb_matrix_set_color(9, 0x00, 0xFF, 0x00);
+          rgb_matrix_set_color(10, 0x00, 0xFF, 0x00);
+          rgb_matrix_set_color(11, 0x00, 0xFF, 0x00);
+          rgb_matrix_set_color(12, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(14, 0x8F, 0x00, 0x00);
+          rgb_matrix_set_color(15, 0xFF, 0xFF, 0x00);
+          rgb_matrix_set_color(16, 0x80, 0x80, 0x00);
+          rgb_matrix_set_color(17, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(18, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(19, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(20, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(21, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(22, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(23, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(24, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(25, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(26, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(27, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(28, 0x8F, 0x00, 0x00);
+          rgb_matrix_set_color(29, 0xC2, 0x50, 0x80);
+          rgb_matrix_set_color(30, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(31, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(32, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(33, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(34, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(35, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(36, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(37, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(38, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(39, 0x00, 0xFF, 0xAA);
+          rgb_matrix_set_color(40, 0x2F, 0x00, 0x00);
+          rgb_matrix_set_color(41, 0xFF, 0xFF, 0xFF);
+          rgb_matrix_set_color(42, 0x00, 0x00, 0xFF);
+          rgb_matrix_set_color(43, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(44, 0xFF, 0xFF, 0x00);
+          rgb_matrix_set_color(45, 0x8F, 0x8F, 0x00);
+          rgb_matrix_set_color(46, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(47, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(48, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(49, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(50, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(51, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(52, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(53, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(54, 0xFF, 0x63, 0x47);
+          rgb_matrix_set_color(55, 0x00, 0x00, 0x8F);
+          rgb_matrix_set_color(56, 0xFF, 0x63, 0x47);
+          rgb_matrix_set_color(57, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(58, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(59, 0x4F, 0x4F, 0xFF);
+          rgb_matrix_set_color(60, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(61, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(62, 0x00, 0x00, 0x00);
+          break;
+	    case _MOD1:
+          rgb_matrix_set_color(14, 0x8F, 0x00, 0x00);
+          rgb_matrix_set_color(15, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(16, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(17, 0x00, 0xFF, 0xFF);
+          rgb_matrix_set_color(18, 0xFF, 0xFF, 0x00);
+          rgb_matrix_set_color(19, 0x00, 0x00, 0xFF);
+          rgb_matrix_set_color(20, 0x00, 0xFF, 0x00);
+          rgb_matrix_set_color(21, 0xFF, 0x00, 0x00);
+          rgb_matrix_set_color(22, 0xFF, 0xFF, 0xFF);
+          rgb_matrix_set_color(23, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(24, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(25, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(26, 0xC2, 0x50, 0x80);
+          rgb_matrix_set_color(27, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(28, 0x8F, 0x00, 0x00);
+          rgb_matrix_set_color(29, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(37, 0xC2, 0x50, 0x80);
+          rgb_matrix_set_color(30, 0x00, 0xA0, 0xA0);
+          rgb_matrix_set_color(36, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(35, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(34, 0xA0, 0x00, 0x00);
+          rgb_matrix_set_color(33, 0x00, 0xA0, 0x00);
+          rgb_matrix_set_color(32, 0x00, 0x00, 0xA0);
+          rgb_matrix_set_color(31, 0xA0, 0xA0, 0x00);
+          rgb_matrix_set_color(38, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(39, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(40, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(41, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(42, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(43, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(44, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(50, 0xC2, 0x50, 0x80);
+          rgb_matrix_set_color(46, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(47, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(48, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(49, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(45, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(51, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(52, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(53, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(54, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(55, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(56, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(57, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(58, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(59, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(60, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(61, 0x00, 0x00, 0x00);
+          rgb_matrix_set_color(62, 0x00, 0x00, 0x00);
+          break;
+      }
+    }
+    if ( this_led & (1<<USB_LED_CAPS_LOCK)) {
+      rgb_matrix_set_color(40, 0xFF, 0xFF, 0xFF);
+    }
+}
+
+void matrix_init_user(void) {
+  //user initialization
+}
+
+void matrix_scan_user(void) {
+  //user matrix
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	  return true;
+	}
---
+++ keebio_viterbi_rev2/j/keymap.c
@@ -0,0 +1,167 @@
+#include QMK_KEYBOARD_H
+
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _QWERTY 0
+#define _COLMAK 1
+#define _DVORAK 2
+#define _LOWER 10
+#define _RAISE 11
+#define _ADJUST 16
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  COLMAK,
+  DVORAK,
+  LOWER,
+  RAISE,
+  ADJUST,
+  CKC_1_12,
+  CKC_2_13
+};
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* QWERTY
+ * ,------------------------------------------------.      ,------------------------------------------------.
+ * |  ESC |   `  |   1  |   2  |   3  |   4  |   5  |      |   6  |   7  |   8  |   9  |   0  |  -   | BSPC |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * | PGUP | Tab  |   Q  |   W  |   E  |   R  |   T  |      |   Y  |   U  |   I  |   O  |   P  |   [  |   ]  |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * | PGDN | CAPS |   A  |   S  |   D  |   F  |   G  |      |   H  |   J  |   K  |   L  |   ;  |   "  |Enter |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |  ALT |SHIFT |   Z  |   X  |   C  |   V  |   B  |      |   N  |   M  |   ,  |   .  |   /  |SHIFT | DEL  |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * | CTRL | WIN  | LEFT | DOWN |LOWER |    Space    |      |    Space    |Raise |  UP  |RIGHT | ALT  | CTRL |
+ * `------------------------------------------------'      `------------------------------------------------'
+ */
+  [_QWERTY] = LAYOUT_ortho_5x14(
+    KC_ESC , KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_BSPC,
+    KC_PGUP, KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_BSLS,
+    KC_PGDN, KC_CAPS, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT ,
+    KC_INS,  KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,     KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_DEL ,
+    KC_LCTL, KC_LGUI, KC_LEFT, KC_DOWN, LOWER  , KC_SPC , KC_SPC ,     KC_SPC , KC_SPC , RAISE  , KC_UP,   KC_RGHT, KC_RALT, KC_RCTL
+  ),
+  [_COLMAK] = LAYOUT_ortho_5x14(
+    KC_ESC , KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_BSPC,
+    KC_PGUP, KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_G   ,     KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_LBRC, KC_BSLS,
+    KC_PGDN, KC_CAPS, KC_A   , KC_R   , KC_S   , KC_T   , KC_D   ,     KC_H   , KC_N   , KC_E   , KC_I   , KC_O   , KC_QUOT, KC_ENT ,
+    KC_INS,  KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,     KC_K   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_DEL ,
+    KC_LCTL, KC_LGUI, KC_LEFT, KC_DOWN, LOWER  , KC_SPC , KC_SPC ,     KC_SPC , KC_SPC , RAISE  , KC_UP  , KC_RGHT, KC_RALT, KC_RCTL
+  ),
+  [_DVORAK] = LAYOUT_ortho_5x14(
+    KC_ESC , KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_LBRC, KC_BSPC,
+    KC_PGUP, KC_TAB , KC_QUOT, KC_COMM, KC_DOT , KC_P   , KC_Y   ,     KC_F   , KC_G   , KC_C   , KC_R   , KC_L   , KC_SLSH, KC_BSLS,
+    KC_PGDN, KC_CAPS, KC_A   , KC_O   , KC_E   , KC_U   , KC_I   ,     KC_D   , KC_H   , KC_T   , KC_N   , KC_S   , KC_MINS , KC_ENT ,
+    KC_INS,  KC_LSFT, KC_SCLN, KC_Q   , KC_J   , KC_K   , KC_X   ,     KC_B   , KC_M   , KC_W   , KC_V   , KC_X   , KC_RSFT, KC_DEL ,
+    KC_LCTL, KC_LGUI, KC_LEFT, KC_DOWN, LOWER  , KC_SPC , KC_SPC ,     KC_SPC , KC_SPC , RAISE  , KC_UP  , KC_RGHT, KC_RALT, KC_RCTL
+  ),
+
+
+
+/* Lower
+ * ,------------------------------------------------.      ,------------------------------------------------.
+ * |      |   ~  |  F1  |  F3  |  F3  |  F4  |  F5  |      |  F6  |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |   _  |      |   !  |   @  |   #  |   $  |   %  |      |   ^  |   &  |   *  |   (  |   )  |   {  |   }  |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |   +  |      |  F1  |  F2  |  F3  |  F4  |  F5  |      |  F6  |   _  |   +  |   {  |   }  |      |      |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |      |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |  F12 |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |      |
+ * `------------------------------------------------'      `------------------------------------------------'
+ */
+  [_LOWER] = LAYOUT_ortho_5x14(
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,       KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
+    _______, _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______,     _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,     _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, KC_INS,
+    _______, KC_LALT,  KC_VOLD, KC_MPRV, _______, KC_MPLY, KC_MPLY,     KC_MPLY, KC_MPLY, _______, KC_VOLU, KC_MNXT, _______, _______
+  ),
+
+/* Raise
+ * ,------------------------------------------------.      ,------------------------------------------------.
+ * |      |   ~  |  F1  |  F3  |  F3  |  F4  |  F5  |      |  F6  |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |   _  |      |   1  |   2  |   3  |   4  |   5  |      |   6  |   7  |   8  |   9  |   0  |   {  |   }  |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |   +  |      |  F1  |  F2  |  F3  |  F4  |  F5  |      |  F6  |   -  |   =  |   [  |   ]  |      |      |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |      |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |  F12 |ISO # |ISO / |      |      |      |      |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |      |
+ * `------------------------------------------------'      `------------------------------------------------'
+ */
+  [_RAISE] = LAYOUT_ortho_5x14(
+    _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, KC_MINS, _______,
+    _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, KC_RBRC,
+    _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, KC_INS,
+    _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______
+  ),
+
+  [_ADJUST] = LAYOUT_ortho_5x14(
+    _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, QWERTY,  _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, RESET,
+    _______, _______, _______, _______, DVORAK,  _______, _______,     _______, _______, _______, _______, _______, _______, EEP_RST,
+    _______, _______, _______, _______, COLMAK,  _______, _______,     _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______
+  )
+
+};
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    //case CKC_1_12:
+    //  switch(get_highest_layer(state)) {
+    //    case DVORAK:
+    //      register_code(KC_RBRC); //is this the right function ???
+    //      break;
+    //    default :
+    //      register_code(KC_MINS);
+    //      break;
+
+    case ADJUST:
+      if (record->event.pressed) {
+        layer_on(_ADJUST);
+      } else {
+        layer_off(_ADJUST);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
+
