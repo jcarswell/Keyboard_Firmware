c2672ae302bf7a4805aef8b9dbbc5126e24b79c3
Josh Carswell <josh.carswell@thecarswells.ca>
2021-10-27 12:44:10 +0000

    dz60 should be dz60 not dz60_rgb_v2_1

--- dz60/j/keymap.c
+++ dz60/j/keymap.c
@@ -1,35 +1,37 @@
 #include QMK_KEYBOARD_H
+
 #define _QWERTY 0
 #define _COLMAK 1
 #define _DVORAK 2
 #define _MOD0 3
 #define _MOD1 4
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-		[_QWERTY] = LAYOUT( /* Base QWERTY */
+		[_QWERTY] = LAYOUT_60_ansi_arrow( /* Base QWERTY */
 			KC_GESC,         KC_1,    KC_2,            KC_3,    KC_4,    KC_5,   KC_6,     KC_7,    KC_8,    KC_9,    KC_0,            KC_MINS, KC_EQL,  KC_BSPC,
 		    KC_TAB,          KC_Q,    KC_W,            KC_E,    KC_R,    KC_T,   KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,            KC_LBRC, KC_RBRC, KC_BSLASH,
 			CTL_T(KC_CAPS),  KC_A,    KC_S,            KC_D,    KC_F,    KC_G,   KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN,         KC_QUOT,          KC_ENT,
 		    KC_LSFT,         KC_Z,    KC_X,            KC_C,    KC_V,    KC_B,   KC_N,     KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), KC_UP,            KC_DEL,
 		    KC_LCTL,        KC_LGUI,  KC_LALT,                   KC_SPC,                    MO(_MOD0),   KC_RCTL, KC_LEFT,         KC_DOWN,          KC_RIGHT),
-		[_COLMAK] = LAYOUT( /* Base COLMAK*/
+		[_COLMAK] = LAYOUT_60_ansi_arrow( /* Base COLMAK*/
 			KC_GRV,          KC_1,    KC_2,            KC_3,    KC_4,    KC_5,   KC_6,     KC_7,    KC_8,    KC_9,    KC_0,            KC_MINS, KC_EQL,  KC_BSPC,
 		    KC_TAB,          KC_Q,    KC_W,            KC_F,    KC_P,    KC_G,   KC_J,     KC_L,    KC_U,    KC_Y,    KC_SCLN,         KC_LBRC, KC_RBRC, KC_BSLASH,
 			CTL_T(KC_CAPS),  KC_A,    KC_R,            KC_S,    KC_T,    KC_D,   KC_H,     KC_N,    KC_E,    KC_I,    KC_O,            KC_QUOT,          KC_ENT,
 		    KC_LSFT,         KC_Z,    KC_X,            KC_C,    KC_V,    KC_B,   KC_K,     KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), KC_UP,            KC_DEL,
 		    KC_LCTL,        KC_LGUI,  KC_LALT,                   KC_SPC,                    MO(_MOD0),   KC_RCTL, KC_LEFT,         KC_DOWN,          KC_RIGHT),
-		[_DVORAK] = LAYOUT( /* Base DVORAK */
+		[_DVORAK] = LAYOUT_60_ansi_arrow( /* Base DVORAK */
 			KC_GESC,         KC_1,    KC_2,            KC_3,    KC_4,    KC_5,   KC_6,     KC_7,    KC_8,    KC_9,    KC_0,            KC_LBRC, KC_RBRC, KC_BSPC,
 		    KC_TAB,          KC_QUOT, KC_COMM,         KC_DOT,  KC_P,    KC_Y,   KC_F,     KC_G,    KC_C,    KC_R,    KC_L,            KC_SLSH, KC_EQL,  KC_BSLASH,
 			CTL_T(KC_CAPS),  KC_A,    KC_O,            KC_E,    KC_U,    KC_I,   KC_D,     KC_H,    KC_T,    KC_N,    KC_S,            KC_MINS,          KC_ENT,
 		    KC_LSFT,         KC_SCLN, KC_Q,            KC_J,    KC_K,    KC_X,   KC_B,     KC_M,    KC_W,    KC_V,    KC_Z,            KC_UP,            KC_RSFT,
 		    KC_LCTL,        KC_LGUI,  KC_LALT,                  KC_SPC,                    MO(_MOD0),   KC_RCTL, KC_LEFT,         KC_DOWN,          KC_RIGHT),
-		[_MOD0] = LAYOUT( /* FN */
+		[_MOD0] = LAYOUT_60_ansi_arrow( /* FN */
 		    KC_GRV,          KC_F1,   KC_F2,           KC_F3,   KC_F4,   KC_F5,  KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,          KC_F11,  KC_F12, _______,
 		    _______,         _______, _______,         _______, _______, _______, _______, _______, _______, _______, _______,         KC_PGDN, KC_PGUP, RESET,
 		    _______,         _______, _______,         _______, _______, _______, _______, _______, _______, _______, _______,         TO(_MOD1),        EEP_RST,
 		    _______,         _______, _______,         _______, _______, _______, _______, _______, KC_HOME, KC_END,  _______,         KC_VOLU,          KC_INS,
 			_______,         _______, _______,                           KC_MPLY,                   _______, _______, KC_MPRV,         KC_VOLD,          KC_MNXT),
-		[_MOD1] = LAYOUT( /* CONTROL */
+		[_MOD1] = LAYOUT_60_ansi_arrow( /* CONTROL */
 			_______,         KC_F1,       KC_F2,       KC_F3,       KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,          KC_F11,  KC_F12, _______,
 			_______,         TO(_QWERTY), _______,     _______,     _______, _______, _______, _______, _______, _______, _______,         _______, _______, RESET,
 			_______,         _______,     TO(_DVORAK), _______,     _______, _______, _______, _______, _______, _______, _______,         _______,          EEP_RST,
@@ -168,7 +170,6 @@ void rgb_matrix_indicators_user(void) {
       rgb_matrix_set_color(40, 0xFF, 0xFF, 0xFF);
     }
 }
-#endif
 
 void matrix_init_user(void) {
   //user initialization
@@ -177,6 +178,7 @@ void matrix_init_user(void) {
 void matrix_scan_user(void) {
   //user matrix
 }
+#endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	  return true;
--- dztech_dz60rgb_v2_1/j2_1/rules.mk
+++
@@ -1,3 +0,0 @@
-BACKLIGHT_ENABLE = no         # Enable keyboard backlight functionality
-RGB_MATRIX_ENABLE = no        # Use RGB matrix
-
